6ccc7f7159596f51989e15186b998bc3da2889ea
Sebastian Buckpesch <s.buckpesch@iconsultants.eu>
2015-04-24 09:06:10 +0200

    Additional headers and curl options for all requests
    Usage of the proposed changes of the Curl-Wrapper class

--- www/index.php
+++ www/index.php
@@ -6,9 +6,18 @@ use Symfony\Component\HttpFoundation\Request;
 use RestProxy\RestProxy;
 use RestProxy\CurlWrapper;
 
+// Example for additional Curl request headers and additional curl options for all requests
+$requestHeaders = array(
+    'Content-Type:application/json',
+    'Authorization: Basic '. base64_encode("username:password")
+);
+$curlOptions = array(
+    CURLOPT_SSL_VERIFYPEER => 0,
+    CURLOPT_SSL_VERIFYHOST => 0
+);
 $proxy = new RestProxy(
     Request::createFromGlobals(),
-    new CurlWrapper()
+    new CurlWrapper($requestHeaders, $curlOptions)
 );
 
 $proxy->register('github/example/With/2/destinations', 'https://api.github.com');
