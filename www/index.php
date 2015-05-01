b6a704eb3ffde07e3c0163aed99b90098511ddbe
Gonzalo Ayuso <gonzalo123@gmail.com>
2015-05-01 17:45:16 +0200

    clean

--- www/index.php
+++ www/index.php
@@ -7,15 +7,15 @@ use RestProxy\RestProxy;
 use RestProxy\CurlWrapper;
 
 // Example for additional Curl request headers and additional curl options for all requests
-$requestHeaders = array(
+$requestHeaders = [
     'Content-Type:application/json',
-    'Authorization: Basic '. base64_encode("username:password")
-);
-$curlOptions = array(
+    'Authorization: Basic ' . base64_encode("username:password")
+];
+$curlOptions    = [
     CURLOPT_SSL_VERIFYPEER => 0,
     CURLOPT_SSL_VERIFYHOST => 0
-);
-$proxy = new RestProxy(
+];
+$proxy          = new RestProxy(
     Request::createFromGlobals(),
     new CurlWrapper($requestHeaders, $curlOptions)
 );
